be1e739e1ca57c9635b7b38f54918ad1c7324d04
Noel <bonanoel1021@gmail.com>
2024-07-22 15:46:58 +0200

    Date caller and responder added
    Date caller and responder added for deployment testing.

---
+++ dateresponder/src/main/java/app/kind/org/dateresponder/controller/ServerController.java
@@ -0,0 +1,18 @@
+package app.kind.org.dateresponder.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class ServerController {
+
+    @GetMapping("/current-date")
+    public String getCurrentDate() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+}
